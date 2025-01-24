f70e2b1a8e4829d3f66dedffd51689f1f1f1807b
Marco MR <marcomr9641@gmail.com>
2025-01-24 14:27:38 -0600

    exercises 4.3 to 4.5

---
+++ utils/list_helper.js
@@ -0,0 +1,28 @@
+const dummy = (blogs) => {
+    return 1
+}
+
+const totalLikes = (blogs) => {
+    const reducer = (sum, item) => {
+      return sum + item.likes
+    }
+    return blogs.length === 0
+      ? 0
+      : blogs.reduce(reducer, 0)
+}
+  
+const favoriteBlog = (blogs) => {
+    const reducer = (max, blog) => {
+        return max.likes > blog.likes ? max : blog;
+    };
+
+    return blogs.length === 0
+        ? null 
+        : blogs.reduce(reducer); 
+};
+
+module.exports = {
+    dummy,
+    totalLikes, 
+    favoriteBlog
+}
